c44a4a7e8b794c1091d3e4dd9403a8375c860c38
AlejandroRC-Github <145007287+AlejandroRC-Github@users.noreply.github.com>
2024-05-13 12:49:12 +0200

    Primera modificación en App.js
    Esta modificación se ha hecho para añadir las rutas a mi página web. Utilización del React Router.
    Los nombres de las paginas y contenido se irán modificando con los siguientes commits.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import HomePage from './components/Home';
+import AboutPage from './components/Sobre_nosotros';
+import ContactPage from './components/Contacto';
+import NotFoundPage from './components/NotFoundPage';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Sobre_Nosotros" component={SobreNosotros} />
+          <Route path="/contacto" component={Contacto} />
+          <Route component={NotFoundPage} /> /*Ruta para página no encontrada*/
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
